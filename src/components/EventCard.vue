4d18dd0e867c95da61ce9446b24187f71fabb42a
Scott <37982316+scottnotscott@users.noreply.github.com>
2022-06-11 22:53:07 +0100

    refactored event component

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -8,22 +8,12 @@
 <script>
 export default {
   name: "EventCard",
-  // props: {
-  //   msg: String,
-  // },
+   props: {
+     event: Object,
+   },
   data() {
     return {
-      event: {
-        id: 5928101,
-        category: "animal welfare",
-        title: "Cat Adoption Day",
-        description: "Find your new feline friend at this event.",
-        location: "Meow Town",
-        date: "July 30, 2022",
-        time: "12:00",
-        petsAllowed: true,
-        organizer: "Kat Laydee",
-      },
+      
     }
   },
 }
---
+++ src/views/Home.vue
@@ -0,0 +1,65 @@
+<template>
+<h1>Events for Good</h1>
+  <div class="events">
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import EventCard from "@/components/EventCard.vue";
+
+export default {
+  name: "Home",
+  components: {
+    EventCard,
+  },
+  data() {
+    return {
+      events: [
+        {
+          id: 184832384,
+          category: 'food',
+          title: 'Community Gardening',
+          description: 'Join us as we tend to the edible community plants.',
+          location: 'Dundee City Centre',
+          date: 'July 30 2022',
+          time: '10:00am',
+          petsAllowed: true,
+          organizer: 'Fern Pollin'
+        },
+        {
+          id: 194727239,
+          category: 'sustainability',
+          title: 'City Cleanup',
+          description: 'Help pick up rubbish across the city.',
+          location: 'Dundee City Wide',
+          date: 'August 1 2022',
+          time: '11:00am',
+          petsAllowed: false,
+          organizer: 'Hates Rubbish'
+        },
+        {
+          id: 3848734737, 
+          category: 'animal welfare',
+          title: 'Big Dog Walk',
+          description: 'Help your dogs make friends across the city.',
+          location: 'Balgay Park, Dundee',
+          date: 'August 2 2022',
+          time: '12:00pm',
+          petsAllowed: true,
+          organizer: 'Snoop Dog'
+        }
+      ]
+    }
+  }
+};
+</script>
+
+<style scoped>
+.events {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
