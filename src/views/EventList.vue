e28c1991929c1b42440394f9e8d710127f7cdc15
Scott <37982316+scottnotscott@users.noreply.github.com>
2022-06-12 19:43:56 +0100

    improve the pagination styling

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -3,19 +3,23 @@
   <div class="events">
     <EventCard v-for="event in events" :key="event.id" :event="event" />
 
+  <div class="pagination">
     <router-link
+    id="page-prev"
       :to="{ name: 'EventList', query: { page: page - 1 }}"
       rel="prev"
       v-if="page != 1"
-      >Prev Page
+      >&#60; Previous
     </router-link>
 
     <router-link
+    id="page-next"
       :to="{ name: 'EventList', query: { page: page + 1 }}"
       rel="next"
       v-if="hasNextPage"
-      >Next Page
+      >Next &#62;
     </router-link>
+    </div>
   </div>
 </template>
 
@@ -65,4 +69,24 @@ export default {
   flex-direction: column;
   align-items: center;
 }
+.pagination {
+  display: flex;
+  width: 290px;
+}
+
+.pagination a {
+  flex: 1;
+  text-decoration: none;
+  color: #2c3e50;
+}
+
+#page-prev {
+  text-align: left;
+}
+
+#page-next {
+  text-align: right;
+}
+
+
 </style>
