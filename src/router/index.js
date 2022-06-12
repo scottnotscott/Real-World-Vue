ad72e027dbaa19c193b4c1b6996282f4b73892d1
Scott <37982316+scottnotscott@users.noreply.github.com>
2022-06-12 20:27:55 +0100

    added nested routes and optimised

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import EventList from "../views/EventList.vue";
 import EventDetails from "../views/event/Details.vue";
 import EventRegister from "../views/event/Register.vue";
 import EventEdit from "../views/event/Edit.vue";
+import EventLayout from "../views/event/Layout.vue";
 import About from '../views/About.vue'
 
 const routes = [
@@ -21,21 +22,26 @@ const routes = [
   },
   {
     path: '/event/:id',
-    name: 'EventDetails',
+    name: 'EventLayout',
     props: true,
-    component: EventDetails,
-  },
-  {
-    path: '/event/:id/register',
-    name: 'EventRegister',
-    props: true,
-    component: EventRegister,
-  },
-  {
-    path: '/event/:id/edit',
-    name: 'EventEdit',
-    props: true,
-    component: EventEdit,
+    component: EventLayout,
+    children: [
+      {
+        path: '', // root path of parent /event/:id
+        name: 'EventDetails',
+        component: EventDetails
+      },
+      {
+        path: 'register', // /event/:id/register
+        name: 'EventRegister',
+        component: EventRegister,
+      },
+      {
+        path: 'edit', // /event/:id/edit
+        name: 'EventEdit',
+        component: EventEdit,
+      },
+    ]
   },
 ];
 
