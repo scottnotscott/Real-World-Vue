6b05b70681fd398313bd5440a4b9a098dc0140e7
Scott <37982316+scottnotscott@users.noreply.github.com>
2022-06-12 13:07:17 +0100

    Some more router refactoring

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,18 @@
 import { createRouter, createWebHistory } from "vue-router";
-import Home from "../views/Home.vue";
+import EventList from "../views/EventList.vue";
+import About from '../views/About.vue'
 
 const routes = [
   {
     path: "/",
-    name: "home",
-    component: Home,
+    name: "EventList",
+    component: EventList,
   },
   {
     path: "/about",
     name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
-  },
+    component: About
+  }
 ];
 
 const router = createRouter({
