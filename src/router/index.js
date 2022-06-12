e70e6521bfeb15773739fa0c8542b6d9c6bc46e5
Scott <37982316+scottnotscott@users.noreply.github.com>
2022-06-12 13:43:10 +0100

    Created EventDetails component, added new api call to fetch single event by id, added component to routes and made eventcard clickable with router-link

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 import EventList from "../views/EventList.vue";
+import EventDetails from "../views/EventDetails.vue";
 import About from '../views/About.vue'
 
 const routes = [
@@ -12,6 +13,11 @@ const routes = [
     path: "/about",
     name: "about",
     component: About
+  },
+  {
+    path: '/event/123',
+    name: 'EventDetails',
+    component: EventDetails,
   }
 ];
 
---
+++ src/views/EventDetails.vue
@@ -0,0 +1,29 @@
+<template>
+<div>
+    <h1> {{event.title}} </h1>
+    <p>{{event.time}} on {{event.date}} @ {{event.location}}</p>
+    <p>{{event.description}}</p>
+</div>
+</template>
+
+<script>
+import EventService from '@/services/EventService'
+
+export default {
+    data() {
+        return {
+            event: null,
+            id: 123
+        }
+    },
+    created() {
+        EventService.getEvent(this.id)
+            .then(response => {
+                this.event = response.data
+            })
+            .catch(error => {
+                console.log(error)
+            })
+    }
+}
+</script>
