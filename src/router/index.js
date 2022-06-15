fc2ae76005aada66d0b68239ef681dd266062d0e
Scott <37982316+scottnotscott@users.noreply.github.com>
2022-06-15 13:43:59 +0100

    Modified progress bar to activate on each route and only stop loading once component loaded, moved data to store. fixes #6

--- src/router/index.js
+++ src/router/index.js
@@ -8,6 +8,8 @@ import About from '../views/About.vue'
 import NotFound from '../views/NotFound.vue'
 import NetworkError from '../views/NetworkError.vue'
 import Nprogress from 'nprogress'
+import EventService from '@/services/EventService'
+import GStore from '@/store'
 
 const routes = [
   {
@@ -28,6 +30,22 @@ const routes = [
     name: 'EventLayout',
     props: true,
     component: EventLayout,
+    beforeEnter: to => {
+      return EventService.getEvent(to.params.id)
+      .then(response => {
+          GStore.event = response.data
+      })
+      .catch(error => {
+          if(error.response && error.response.status == 404) {
+              return ({
+                  name: '404Resource',
+                  params: { resource: 'event'}
+              })
+          } else {
+              return ({ name: 'NetworkError'})
+          }
+      })
+    },
     children: [
       {
         path: '', // root path of parent /event/:id
